f3808a2a3c942587dec4b01ad53ca1ccc4a45846
toshi85 <tabibito1207@gmail.com>
2020-02-02 15:02:17 +0900

    syuusei2

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,18 +1,18 @@
-# class ImageUploader < CarrierWave::Uploader::Base
+class ImageUploader < CarrierWave::Uploader::Base
 #   # Include RMagick or MiniMagick support:
 #   # include CarrierWave::RMagick
-#   include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
   
-#   process resize_to_fit: [800,800]
+  process resize_to_fit: [800,800]
 #   # Choose what kind of storage to use for this uploader:
-#   storage :file
+  storage :file
 #   # storage :fog
 
 #   # Override the directory where uploaded files will be stored.
 #   # This is a sensible default for uploaders that are meant to be mounted:
-#   def store_dir
-#     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-#   end
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
 
 #   # Provide a default URL as a default if there hasn't been a file uploaded:
 #   # def default_url(*args)
@@ -45,4 +45,4 @@
 #   # def filename
 #   #   "something.jpg" if original_filename
 #   # end
-# end
+end
