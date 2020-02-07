e66e523727ad9400d4f502a3d1cd88c7daef3cd5
toshi85 <tabibito1207@gmail.com>
2020-02-07 17:59:13 +0900

    syuusei7

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -84,7 +84,7 @@ $(function(){
                ${message.user_name}
              </div>
              <div class="upper-message__date">
-               ${message.created_at.strftime("%Y年%m月%d日 %H時%M分"}
+               ${message.created_at.strftime("%Y年%m月%d日 %H時%M分")}
              </div>
            </div>
            <div class="lower-message">
