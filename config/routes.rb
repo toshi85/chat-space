c1c6c105138c1daef358955bc22b0fd9a16d807d
toshi85 <tabibito1207@gmail.com>
2020-02-07 18:01:13 +0900

    syuusi8

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'groups#index'
   resources :users, only: [:index, :edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update] do
+  resources :groups, only: [:index, :new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
-  get 'messages/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "messages#index"
