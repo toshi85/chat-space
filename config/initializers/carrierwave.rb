649904aa13070a141b5e70176e52adc6f035d339
toshi85 <tabibito1207@gmail.com>
2020-02-09 21:34:49 +0900

    aaa

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,7 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'ここにバケット名を入れます'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ここにバケット名を入れます'
+  config.fog_directory  = 'tosimasa'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/tosimasa'
+
 end
